41e8644be5b89d75701c13235c164974aed6f10c
ltheod01 <80636546+ltheod01@users.noreply.github.com>
2021-03-14 18:31:00 +0200

    Delete twitter_credentials.py

--- twitter_credentials.py
+++
@@ -1,7 +0,0 @@
-#Variables that contain the user credentials to access the api
-API="...."
-API_TOKEN_SECRET="....."
-CONSUMER_KEY="..."
-CONSUMER_SECRET="..."
-ACCESS_TOKEN="..."
-ACCESS_TOKEN_SECRET="..."

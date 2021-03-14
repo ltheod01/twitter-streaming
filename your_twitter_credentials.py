85806bf9c88f2772e03975b361cdba9f8eefa0db
ltheod01 <80636546+ltheod01@users.noreply.github.com>
2021-03-14 18:34:34 +0200

    Add files via upload

---
+++ your_twitter_credentials.py
@@ -0,0 +1,7 @@
+#Variables that contain the user credentials to access the api
+API="..."
+API_TOKEN_SECRET="..."
+CONSUMER_KEY="..."
+CONSUMER_SECRET="..."
+ACCESS_TOKEN="..."
+ACCESS_TOKEN_SECRET="..."
